328b5d49e5b0ceef0099b0c4b3e81814bc791928
Panos-laptop <panosevage@yahoo.com>
2018-03-01 12:01:03 +0200

    initial versions / template of source and header files

---
+++ postingList/docInfo.h
@@ -0,0 +1,12 @@
+/***************************************************************************************
+Header file : docInfo.h
+Programmer	: PANAGIOTIS EVANGELIOU  AM:1115201500039
+Description :
+
+
+
+****************************************************************************************/
+#ifndef DOC_INFO_H
+#define DOC_INFO_H
+
+#endif /* DOC_INFO_H */
---
+++ postingList/postingList.h
@@ -0,0 +1,12 @@
+/***************************************************************************************
+Header file : postingList.h
+Programmer	: PANAGIOTIS EVANGELIOU  AM:1115201500039
+Description :
+
+
+
+****************************************************************************************/
+#ifndef POST_LIST_H
+#define POST_LIST_H
+
+#endif /* POST_LIST_H */
